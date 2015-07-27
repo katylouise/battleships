d8023b35d49f455cf12d9c2ad7ac9c2897e01828
Leon Wee <leonwee27@gmail.com>
2015-07-27 16:28:44 +0700

    Deleted one test in board

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -13,14 +13,14 @@ describe Board do
   #   expect(subject.place(ship)).to eq ship
   # end
 
-  it "when we place a ship it is added to the grid" do
-   # allow(ship).to receive(:location)
-    location = [0, 0]
-    subject.place(ship, location)
-    expect(subject.grid).not_to be_empty
-  end
+  # it "when we place a ship it is added to the grid" do
+  #  # allow(ship).to receive(:location)
+  #   location = [0, 0]
+  #   subject.place(ship, location)
+  #   expect(subject.grid).not_to be_empty
+  # end
 
-  it "has placed a ship at the given location" do
+  it "has placed a ship in the grid" do
     location = [0, 0]
     subject.place(ship, location)
     expect(subject.grid).to include([ship])
